8251aa377035ac1c3d6cc76a41ebe47eded8821d
Hamnah Sabry <hamnahsabry@gmail.com>
2020-12-22 19:27:05 +0100

    Placing duxks randomly

--- duckgame.py
+++ duckgame.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
@@ -10,12 +10,22 @@ cloud1 = pygame.image.load('Cloud1.png')
 cloud2 = pygame.image.load('Cloud2.png')
 water_bg = pygame.image.load('Water_BG.png')
 crosshair = pygame.image.load('crosshair.png')
-
+crosshair_rect=(0,0)
+duck_surface = pygame.image.load('duck.png')
 
 land_position_y = 560
 land_speed = 1
 water_position_y = 640
 water_speed = 1.5
+
+
+duck_list = []
+for duck in range(20):
+    duck_position_x = random.randrange(50,1200)
+    duck_position_y = random.randrange(120,600)
+    duck_rect = duck_surface.get_rect(center = (duck_position_x, duck_position_y))
+    duck_list.append(duck_rect)
+
 while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -25,6 +35,10 @@ while True:
         crosshair_rect = crosshair.get_rect(center = event.pos)
 
     screen.blit(wood_bg,(0,0))
+    for duck_rect in duck_list:
+        screen.blit(duck_surface,duck_rect)
+
+
     land_position_y -= land_speed
     if land_position_y <= 520 or land_position_y >= 600:
         land_speed *= -1
@@ -34,6 +48,7 @@ while True:
     if water_position_y <= 620 or water_position_y >= 680:
         water_speed *= -1
     screen.blit(water_bg,(0,water_position_y))
+
 
     screen.blit(crosshair,crosshair_rect)
     screen.blit(cloud1,(10,10))
